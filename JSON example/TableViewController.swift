24b5fa7343caa593f767051a1385c97e7c48e5d6
dmptv <kanat@Dimas-MacBook-Air.local>
2017-06-16 19:03:10 +0600

    z

--- JSON example/TableViewController.swift
+++ JSON example/TableViewController.swift
@@ -17,7 +17,7 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        getLatestLoans() //
+        getLatestLoans() 
         
         // настроим самомасшабирование высоты ячейки
         tableView.estimatedRowHeight = 120.0
@@ -43,9 +43,9 @@ class TableViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120.0
-    }
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 120.0
+//    }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
